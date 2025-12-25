0ab9bb8a1acee5375b5c889db7ba9fb187621483
ManhKenZ <acc1discordloner@gmail.com>
2025-12-25 20:27:12 +0700

    moTa

---
+++ app.js
@@ -0,0 +1,17 @@
+var fut = new Date("Feb 16, 2026 23:59:59").getTime();
+let x = setInterval(function stime(){
+    var now = new Date().getTime()
+    var D = fut - now
+    var days = Math.floor(D/(1000*60*60*24))
+    var hours = Math.floor(D/(1000*60*60))
+    var minutes = Math.floor(D/(1000*60))
+    var seconds = Math.floor(D/(1000))
+    hours %=24
+    minutes %=60
+    seconds %=60
+    document.getElementById("days").innerText = days
+    document.getElementById("hours").innerText = hours
+    document.getElementById("minutes").innerText = minutes
+    document.getElementById("seconds").innerText = seconds
+
+}, 0.1);
---
+++ data.js
@@ -0,0 +1,14 @@
+const data_name = [
+    {
+        ma_ten: "manh",
+        name: "Mạnh",
+        message: "Hello, I'm Mạnh",
+        img: "https://vcdn1-giaitri.vnecdn.net/2014/11/12/12-1415753927.jpg?w=1200&h=0&q=100&dpr=1&fit=crop&s=eeNdzX-KP7izXlmIm_xMCg"
+    },
+    {
+        ma_ten: "kenz",
+        name: "KenZ",
+        message: "Hello, I'm KenZ",
+        img: "https://vcdn1-giaitri.vnecdn.net/2014/11/12/12-1415753927.jpg?w=1200&h=0&q=100&dpr=1&fit=crop&s=eeNdzX-KP7izXlmIm_xMCg"
+    }
+]
